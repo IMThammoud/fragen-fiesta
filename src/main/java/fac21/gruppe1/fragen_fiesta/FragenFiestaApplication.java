5945f4a465ba649b7d53d18380f0b154d7035e3c
hassan <trondl-github@proton.me>
2024-12-18 12:21:03 +0100

    first commit

---
+++ src/main/java/fac21/gruppe1/fragen_fiesta/FragenFiestaApplication.java
@@ -0,0 +1,13 @@
+package fac21.gruppe1.fragen_fiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragenFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragenFiestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fac21/gruppe1/fragen_fiesta/FragenFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package fac21.gruppe1.fragen_fiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragenFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
