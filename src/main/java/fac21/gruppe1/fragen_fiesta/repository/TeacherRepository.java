f8fdaf78a3225e5db10b6e4903ce6a1f75228575
Alexander Kreb <a.kreb@outlook.com>
2025-01-14 14:21:55 +0100

    Teacher Repository hinzugefügt

---
+++ src/main/java/fac21/gruppe1/fragen_fiesta/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package fac21.gruppe1.fragen_fiesta.repository;
+
+import fac21.gruppe1.fragen_fiesta.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
